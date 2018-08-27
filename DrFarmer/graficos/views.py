22852338103f3ee111b0b1b9bc2177ea8e6930ab
Fabinho <fabio_schezaro@hotmail.com>
2018-08-26 22:50:27 -0300

    inclusão do bootstrap e melhorias no layout

--- DrFarmer/graficos/views.py
+++ DrFarmer/graficos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from random import randint
 
 def index(request):
-    return render(request, 'graficos.html', {"valorEixoY": 15})
+    return render(request, 'graficos.html', {"valorEixoY": randint(15, 25)})
